103c7be5a430815358a17efbb8bd9285fe4a27bd
Neeko ! ! <61304515+Neeko-onTheRoad@users.noreply.github.com>
2025-07-24 23:59:53 +0900

    Projectile Unused Field Error Disable

--- Assets/File_Neeko/GunSystem/ProjectileSystem/Projectile.cs
+++ Assets/File_Neeko/GunSystem/ProjectileSystem/Projectile.cs
@@ -1,8 +1,27 @@
+using UnityEngine;
+
 namespace Neeko {
 
-	public abstract class Projectile {
-		
-		
+	public abstract class Projectile : MonoBehaviour {
+
+		//======================================================================| Inspector Fields
+
+		#pragma warning disable
+
+		[SerializeField]
+		private float _initialSpeed = 1f;
+
+		[SerializeField]
+		private float _bulletSpread = 0f;
+
+		#pragma warning restore
+
+		//======================================================================| Methods
+
+		public virtual void OnShoot() {}
+		public virtual void OnUpdate() {}
+		public virtual void OnHit() {}
+		public virtual void OnDestroyed () {}
 
 	}
 
